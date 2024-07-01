3df8bf04ceab83aad04cc6908a106575b321118b
admin <admin@ghurcka.local>
2024-07-01 12:07:25 -0400

    DOM

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -8,29 +8,84 @@
 
 */
 
+// Referencias al DOM
+const btnPedir = document.querySelector("#btn-pedir");
+const btnNuevo = document.querySelector("#btn-nuevo");
+const btnDetener = document.querySelector("#btn-detener");
+let score = document.querySelector('small');
+
+// Variables
+let valorJugador = 0, valorComputador = 0;
 let deck = [];
-const tipos = ["C","D","H","S"]
-const mayores = ["A","J","Q","K"]
+const tipos = ["C", "D", "H", "S"];
+const mayores = ["A", "J", "Q", "K"];
 
-
-
-const creaDeck = ()=>{
-
-    for (let i=2;i<=10;i++){
-        for (let tipo of tipos){
-            deck.push(i+tipo)
-        }
+const creaDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
     }
+  }
 
-    for (let mayor of mayores){
-        for (let tipo of tipos){
-            deck.push(mayor+tipo)
-        }
+  for (let mayor of mayores) {
+    for (let tipo of tipos) {
+      deck.push(mayor + tipo);
     }
+  }
 
-    console.log(deck)
-    deck = _.shuffle(deck)
-    console.log(deck)
-}
+  deck = _.shuffle(deck);
 
-creaDeck();
+  return deck;
+};
+
+desk = creaDeck();
+
+const getCard = () => {
+  // if (arr && arr.length){
+  //     const indice = Math.floor(Math.random()*arr.length);
+  //     const  card = arr[indice];
+  //     arr.splice(indice,1);
+  //     return {card, arr}
+
+  // }
+
+  if (deck.length === 0) {
+    throw "No hay cartas";
+  }
+
+  const card = deck.pop();
+  return card;
+};
+
+
+
+const valorCarta = (carta) => {
+    let puntos = 0;
+  const valor = carta.substring(0, carta.length - 1);
+  console.log({ valor });
+//   if (isNaN(valor)) {
+//     console.log("No es un numero");
+//     puntos = (valor === 'A')? 11 : 10;
+//   } else {
+//     console.log("Es un numero");
+//     puntos = valor*1;
+//   }
+//   console.log(puntos);
+
+  return puntos = isNaN(valor) ? 
+          (valor === 'A')? 11 : 10 
+          : valor * 1 ;
+   
+};
+
+
+
+// Eventos
+
+btnPedir.addEventListener('click',()=>{
+    const carta = getCard();
+    valor = valorCarta(carta);
+    valorJugador = valorJugador + valor;
+    score.innerText = valorJugador;
+
+})
